9972ae2ff70729cd527c7bb6a1ad7dddd411dd4c
Brian Lamar <brian.lamar@rackspace.com>
2011-10-04 18:12:15 -0400

    Save.

---
+++ src/openstack/common/wsgi/base.py
@@ -0,0 +1,34 @@
+import webob.dec
+
+
+class Request(webob.Request):
+    pass
+
+
+class Middleware(object):
+    """WSGI Middleware."""
+
+    def __init__(self, application):
+        self.application = application
+
+    def process_request(self, req):
+        """Called on each request.
+
+        If this returns None, the next application down the stack will be
+        executed. If it returns a response then that response will be returned
+        and execution will stop here.
+
+        """
+        return None
+
+    def process_response(self, response):
+        """Do whatever you'd like to the response."""
+        return response
+
+    @webob.dec.wsgify(RequestClass=Request)
+    def __call__(self, req):
+        response = self.process_request(req)
+        if response:
+            return response
+        response = req.get_response(self.application)
+        return self.process_response(response)
---
+++ tests/test_wsgi.py
@@ -0,0 +1,64 @@
+# Copyright 2011 OpenStack, LLC
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import subprocess
+
+import unittest2 as unittest
+import webob.dec
+
+import openstack.common.wsgi.base
+
+
+class MiddlewareTestCase(unittest.TestCase):
+    """Tests checking WSGI middleware."""
+
+    def setUp(self):
+        """Run before each test."""
+        self.request = webob.Request.blank("/")
+
+        @webob.dec.wsgify
+        def app(request):
+            return webob.Response("test_middleware")
+
+        self.middleware = openstack.common.wsgi.base.Middleware(app)
+
+    def test_middleware(self):
+        expected = "test_middleware"
+        actual = self.middleware(self.request).body
+
+        self.assertEqual(expected, actual)
+
+    def test_middleware_process_request(self):
+        @webob.dec.wsgify
+        def process_request(request):
+            return webob.Response("process_request")
+
+        self.middleware.process_request = process_request
+        expected = "process_request"
+        actual = self.middleware(self.request).body
+
+        self.assertEqual(expected, actual)
+
+    def test_middleware_process_response(self):
+        @webob.dec.wsgify
+        def process_response(request):
+            return webob.Response("process_response")
+
+        self.middleware.process_response = process_response
+        expected = "process_response"
+        actual = self.middleware(self.request).body
+
+        self.assertEqual(expected, actual)
